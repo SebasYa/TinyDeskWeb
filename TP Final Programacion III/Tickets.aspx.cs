0a1220647714998173436adb39d2c54aa626c552
Max <maxibian16@gmail.com>
2026-06-15 14:50:30 -0300

    Se agrega diseño a Ticket

--- TP Final Programacion III/Tickets.aspx.cs
+++ TP Final Programacion III/Tickets.aspx.cs
@@ -255,5 +255,34 @@ namespace TP_Final_Programacion_III
         protected void btnDesactivar_Click(object sender, EventArgs e)
         {
         }
+
+        protected void dgvTickets_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "VerDetalle")
+            {
+                int idTicket = int.Parse(e.CommandArgument.ToString());
+                Response.Redirect("Tickets.aspx?id=" + idTicket, false);
+            }
+        }
+
+        public string GetClassEtiquetaEstado(object estadoNombre)
+        {
+            if (estadoNombre == null) return "badge text-bg-secondary px-3 py-2";
+            string estado = estadoNombre.ToString().ToLower();
+            if (estado == "en progreso") return "badge text-bg-primary px-3 py-2";
+            if (estado == "finalizado") return "badge text-bg-success px-3 py-2";
+            if (estado == "pendiente") return "badge text-bg-warning px-3 py-2";
+            return "badge text-bg-dark px-3 py-2";
+        }
+
+        public string GetClassEtiquetaPrioridad(object prioridadNombre)
+        {
+            if (prioridadNombre == null) return "badge text-bg-secondary px-3 py-2";
+            string prioridad = prioridadNombre.ToString().ToLower();
+            if (prioridad == "alta") return "badge text-bg-danger px-3 py-2";
+            if (prioridad == "media") return "badge text-bg-warning px-3 py-2";
+            if (prioridad == "baja") return "badge text-bg-success px-3 py-2";
+            return "badge text-bg-secondary px-3 py-2";
+        }
     }
 }
