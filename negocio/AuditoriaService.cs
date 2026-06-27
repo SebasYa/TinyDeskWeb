8e2ff273382020bb5196d4d427816a0a744277eb
Marcelo Rearte Becerra <74743470+marceloreart@users.noreply.github.com>
2026-06-27 20:45:06 -0300

    add Clase AuditoriaService y metodo registrar()

---
+++ negocio/AuditoriaService.cs
@@ -0,0 +1,31 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    public class AuditoriaService
+    {
+        private AuditoriaNegocio auditoriaNegocio = new AuditoriaNegocio();
+
+        public void Registrar(int idUsuario, string entidad, int entidadId, string accion,
+                              string campo, string valorAnterior, string valorNuevo, string descripcion)
+        {
+            Auditoria log = new Auditoria();
+            log.Usuario = new Usuario { Id = idUsuario };
+            log.Entidad = entidad;
+            log.EntidadId = entidadId;
+            log.Accion = accion; // INSERT, UPDATE, DELETE
+            log.CampoModificado = campo;
+            log.ValorAnterior = valorAnterior;
+            log.ValorNuevo = valorNuevo;
+            log.Descripcion = descripcion;
+            log.Fecha = DateTime.Now;
+
+            auditoriaNegocio.Agregar(log);
+        }
+    }
+}
